fd3642864e8c14d2f0c407ed6c84ef511c475771
voidaryan-coder <sharmajiglbajaj19825@gmail.com>
2026-06-08 15:49:36 +0530

    Day-8 uploaded

--- Day-8/que31.c
+++ Day-8/que31.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-  printf("The half pyramid pattern is:\n");
+  printf("The character triangle is:\n");
   for (int i = 1; i <= 5; i++) {
     for (int j = 1; j <= i; j++) {
       printf("%c", j + 64);
